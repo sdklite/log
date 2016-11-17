98cff50e92cd9eeaee9457a0d6903d519e7e8193
johnsonlee <g.johnsonlee@gmail.com>
2016-11-18 01:53:14 +0800

    Fix unit test

--- src/test/java/com/sdklite/log/TestLoggerFactory.java
+++ src/test/java/com/sdklite/log/TestLoggerFactory.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class TestLoggerFactory {
 
-    @Test
+    @Test(expected = RuntimeException.class)
     public void test_obtain_logger() {
         final Logger logger = LoggerFactory.getLogger();
         logger.debug("{0} {1}", "Oops!", new RuntimeException());
