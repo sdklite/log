4ac4f15b25b853c55810159d01a9db90cfd862de
johnsonlee <g.johnsonlee@gmail.com>
2016-11-18 11:15:26 +0800

    Update javadoc comments

--- src/main/java/com/sdklite/log/bind/LoggerBinder.java
+++ src/main/java/com/sdklite/log/bind/LoggerBinder.java
@@ -3,6 +3,13 @@ package com.sdklite.log.bind;
 import com.sdklite.log.Level;
 import com.sdklite.log.Logger;
 
+/**
+ * A stub class for logger implementation binding, the logger implementation
+ * must implements the stub methods.
+ * 
+ * @author johnsonlee
+ *
+ */
 public class LoggerBinder {
 
     public static final LoggerBinder getInstance() {
